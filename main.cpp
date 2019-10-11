ffecdbb83fe58c29a2f817da4fac189f6d90c5d3
simon-gonand <simon.gonand@etu.univ-amu.fr>
2019-10-11 10:35:32 +0000

    initialize window

--- main.cpp
+++ main.cpp
@@ -5,7 +5,44 @@
 
 using namespace std;
 
+SDL_Window* setupSDL(SDL_GLContext& context) {
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		cout << "SDL_Init Error: " << SDL_GetError() << endl;
+		exit(1);
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+	SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
+
+	SDL_Window* window = SDL_CreateWindow("Class test Project", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+	if (window == nullptr) {
+		cout << "SDL_CreateWindow Error: " << SDL_GetError() << endl;
+		SDL_Quit();
+		exit(1);
+	}
+
+	context = SDL_GL_CreateContext(window);
+
+	return window;
+}
+
 int main(int argc, char* argv[]) {
-	SDL_Window* hWindow;
+	SDL_Window* window;
+	SDL_GLContext context;
+	window = setupSDL(context);
+
+	bool finish = false;
+	SDL_Event events;
+	while (!finish) {
+		SDL_WaitEvent(&events);
+		const Uint8* keys = SDL_GetKeyboardState(NULL);
+
+		if (events.window.event == SDL_WINDOWEVENT_CLOSE || keys[SDL_SCANCODE_ESCAPE])
+			finish = true;
+	}
+
 	return 0;
 }
